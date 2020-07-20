b7dc949bd8170d226889b78f3b31ebcf7a500ee6
ajay-waghmare <64916965+ajay-waghmare@users.noreply.github.com>
2020-07-20 14:35:17 +0530

    Add files via upload

---
+++ DAY4/Q5.js
@@ -0,0 +1,3 @@
+do {
+    a = window.prompt('Enter a number');
+} while (a <= 100);
---
+++ DAY4/Q7.js
@@ -0,0 +1,9 @@
+n = 10;
+
+for (i = 2; i <= n; i++) {
+    for (i = 2; i <= n; i++) {
+        if (i % j == 0) {
+            console.log(i)
+        }
+    }
+}
